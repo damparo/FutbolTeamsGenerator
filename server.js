82ad5cd21daf36583b1cf800c2430631a3af3c9b
Dennis Amparo <dennisamparo11@gmail.com>
2020-12-29 18:46:27 -0700

    serving home page

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// app.use(express.static("/public"));
+app.use("/public", express.static("public"))
+
+// Parse application body as JSON
+// middleware - backend
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/catsController.js");
+
+// app.use(routes);
+app.get("/home", function(req, res) {
+    res.render("welcome");
+  });
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
