076677de9bcef4de0e97243800ec64e76acb57b9
Dennis Amparo <dennisamparo11@gmail.com>
2021-02-15 22:48:09 -0700

    set up to deploy to heroku

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const PORT = process.env.PORT || 3600;
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+};
+
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 // app.use(express.static("/public"));
 app.use("/public", express.static("public"))
 
