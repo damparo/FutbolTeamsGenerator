4c0ea9969b2ebfd79b5404017619e55e54d84593
Dennis Amparo <dennisamparo11@gmail.com>
2021-02-17 12:30:43 -0700

    fixed port

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
       host: "localhost",
-      port: 3306,
+      port: 3300,
       user: "root",
       password: "",
       database: "futbol_roster_db",
@@ -19,7 +19,7 @@ if (process.env.JAWSDB_URL) {
 
 // const connection = mysql.createConnection({
 //   host: "localhost",
-//   port: 3306,
+//   port: 3300,
 //   user: "root",
 //   password: "",
 //   database: "futbol_roster_db",
