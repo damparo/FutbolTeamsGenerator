ce472bfb4271b9b24197e96bba917604bfab3201
Dennis Amparo <dennisamparo11@gmail.com>
2021-02-17 11:02:25 -0700

    configuring jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,31 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "futbol_roster_db",
-  multipleStatements: true,
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "futbol_roster_db",
+    multipleStatements: true,
+  });
+};
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "futbol_roster_db",
+//   multipleStatements: true,
+// });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
