b75cea3aa2d5c701e76d4bc4a1fe6a872cd3d82b
Dennis Amparo <dennisamparo11@gmail.com>
2021-06-18 15:16:20 -0700

    fixed gif landing page'

--- public/assets/js/welcome.js
+++ public/assets/js/welcome.js
@@ -1,28 +1,31 @@
 const landingGif = $("#welcome");
 
 // Storing our giphy API URL for a random messi image
-const queryURL =
-  "https://api.giphy.com/v1/gifs/random?api_key=fNB3DLC6bFRFCtlcJ3MWYBuemtukLu5O&tag=messi";
+// const queryURL =
+//   "https://api.giphy.com/v1/gifs/random?api_key=fNB3DLC6bFRFCtlcJ3MWYBuemtukLu5O&tag=fcbarcelona-messi-leo";
 
 // Perfoming an AJAX GET request to our queryURL
-$.ajax({
-  url: queryURL,
-  method: "GET",
-})
+// $.ajax({
+//   url: queryURL,
+//   method: "GET",
+// })
 
   // After the data from the AJAX request comes back
-  .then(function (response) {
-      console.log(response);
+//   .then(function (response) {
+//       console.log(response);
     // Saving the image_original_url property
-    var imageUrl = response.data.image_original_url;
+    // var imageUrl = response.data.image_original_url;
+
+    const imageUrl = "https://media.giphy.com/media/TjAcxImn74uoDYVxFl/giphy.gif";
 
     // Creating and storing an image tag
-    var messiImage = $("<img>");
+    const messiImage = $("<img>");
 
     // Setting the catImage src attribute to imageUrl
-    messiImage.attr("src", imageUrl);
+    messiImage.attr("src", imageUrl).width('380px')
+    .height('380px');
     messiImage.attr("alt", "messi image");
 
     // Prepending the catImage to the images div
     landingGif.prepend(messiImage);
-  });
+//   });
